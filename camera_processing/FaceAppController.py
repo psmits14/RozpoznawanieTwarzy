065f3f7949aa4a176a704e905396fbe17ed0a804
psmits14 <272940@student.pwr.edu.pl>
2025-05-17 12:54:06 +0200

    Działający suwak i pauza w wideo 2

--- camera_processing/FaceAppController.py
+++ camera_processing/FaceAppController.py
@@ -1,48 +1,59 @@
 from PyQt6.QtCore import QTimer
 from camera_processing.FaceApp import FaceApp
-from camera_processing.VideoSource import VideoFileSource
+from camera_processing.VideoSource import VideoFileSource, CameraSource
 
 class FaceAppController:
     def __init__(self, logger, face_app_ui, video_source):
         self.logger = logger
         self.face_app_ui = face_app_ui
         self.face_app = FaceApp(logger, face_app_ui, video_source)
+        self.video_source = video_source
 
-        # Połączenia dla wideo
+        # Slider drag state
+        self.face_app_ui._slider_being_dragged = False
+
+        # --- Połączenia dla VideoFileSource ---
         if isinstance(video_source, VideoFileSource):
             self.face_app_ui.play_pause_btn.clicked.connect(self._toggle_play_pause)
-            self.face_app_ui.video_slider.sliderReleased.connect(self._on_slider_released)
             self.face_app_ui.video_slider.sliderPressed.connect(self._on_slider_pressed)
+            self.face_app_ui.video_slider.sliderReleased.connect(self._on_slider_released)
 
             self.face_app_ui.set_video_fps(video_source._fps)
             self.face_app_ui.video_slider.setMaximum(video_source.get_frame_count())
 
-        # QTimer
+        # --- QTimer ---
         self.timer = QTimer()
         self.timer.timeout.connect(self._update)
 
         if isinstance(video_source, VideoFileSource):
-            target_fps = min(30, video_source._fps)
-            interval = max(10, int(1000 / target_fps))  # 10–33ms
+            fps = min(30, video_source._fps)
+            self.interval = max(10, int(1000 / fps))
         else:
-            interval = 33  # Kamera – standardowo 30 FPS
+            self.interval = 33  # Dla kamery
 
-        self.timer.start(interval)
+        self.timer.start(self.interval)
 
     def _update(self):
-        if not self.face_app.video_source.paused:
+        # Kamera: zawsze aktualizuj
+        # Wideo: tylko jeśli nie zapauzowane
+        if isinstance(self.video_source, CameraSource):
             self.face_app.update()
+        elif isinstance(self.video_source, VideoFileSource):
+            if not self.video_source.paused:
+                self.face_app.update()
 
-        if isinstance(self.face_app.video_source, VideoFileSource):
-            current_frame = self.face_app.video_source.get_current_frame_position()
-            total_frames = self.face_app.video_source.get_frame_count()
-            self.face_app_ui.update_video_controls(current_frame, total_frames)
+            current_frame = self.video_source.get_current_frame_position()
+            total_frames = self.video_source.get_frame_count()
+
+            if not self.face_app_ui._slider_being_dragged:
+                self.face_app_ui.update_video_controls(current_frame, total_frames)
 
     def _toggle_play_pause(self):
-        self.face_app.video_source.toggle_pause()
-        self.face_app_ui.play_pause_btn.setText(
-            "Odtwarzaj" if self.face_app.video_source.paused else "Pauza"
-        )
+        if isinstance(self.video_source, VideoFileSource):
+            self.video_source.toggle_pause()
+            self.face_app_ui.play_pause_btn.setText(
+                "Odtwarzaj" if self.video_source.paused else "Pauza"
+            )
 
     def _on_slider_pressed(self):
         self.face_app_ui._slider_being_dragged = True
@@ -50,4 +61,4 @@ class FaceAppController:
     def _on_slider_released(self):
         self.face_app_ui._slider_being_dragged = False
         frame_num = self.face_app_ui.video_slider.value()
-        self.face_app.video_source.set_frame_position(frame_num)
+        self.video_source.set_frame_position(frame_num)
